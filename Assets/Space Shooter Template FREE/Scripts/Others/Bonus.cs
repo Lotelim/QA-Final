9420665d93f9bc188719c77d5a93afe0f0090af2
ZeUnlucky <tohar77@hotmail.com>
2026-07-25 19:09:01 +0300

    organized

--- Assets/Space Shooter Template FREE/Scripts/Others/Bonus.cs
+++ Assets/Space Shooter Template FREE/Scripts/Others/Bonus.cs
@@ -1,16 +1,13 @@
 ﻿using UnityEngine;
 
-public class Bonus : MonoBehaviour {
-
-    //when colliding with another object, if another objct is 'Player', sending command to the 'Player'
+public class Bonus : MonoBehaviour 
+{
     private void OnTriggerEnter2D(Collider2D collision) 
     {
         if (collision.tag == "Player") 
         {
             if (PlayerShooting.instance.weaponPower < PlayerShooting.instance.maxweaponPower)
-            {
                 PlayerShooting.instance.weaponPower++;
-            }
             Destroy(gameObject);
         }
     }
